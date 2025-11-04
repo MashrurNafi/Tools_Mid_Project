830d0a5f54be692494d774b00c073da58d21014e
MasadRayan <masadrayan2002@gmail.com>
2025-11-04 21:57:18 +0600

    Banner slider added

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 let products = document.querySelector('.productContainer');
 
 let cartQuantity = 0;
+document.querySelector('.cart-quantity').textContent = cartQuantity;
 
 // Fetching Products from Fake Store API
 fetch('https://fakestoreapi.com/products')
-  .then(res=>res.json())            
-    .then((data)=>{
-        products.innerHTML = "";
-        data.forEach(product=>{
-            products.innerHTML += `
-              <div class="product w-[250px]  bg-white-400 border border-gray-200 rounded-lg shadow-lg key=${product.id}">
-                <img src="${product.image}" alt="${product.title}" class="p-10 w-full h-[250px] object-contain">
-                <div class="content flex flex-col p-4 justify-between h-[250px]">
-                    <span class="font-xl text-gray-400">${product.category}</span>
-                    <h3 class="font-bold">${product.title}</h3>
-                    <span class="price">$${product.price}</span>
-                    <button class="js-add-to-cart bg-yellow-400 border rounded w-30 ml-12 hover:cursor-pointer">Add To Cart</button>
-                </div>
+  .then(res => res.json())
+  .then((data) => {
+    products.innerHTML = "";
+    data.forEach(product => {
+      products.innerHTML += `
+              <div class="bg-white p-6 rounded-xl shadow hover:shadow-lg transition text-center">
+            <img src="${product.image}" alt="${product.title}" class="h-40 w-full object-contain mb-4"/>
+            <h3 class="font-semibold text-lg mb-2">${product.title}</h3>
+            <p class="text-[#FF6F3C] font-bold mb-2">${(product.price * 120).toFixed(2)} BDT</p>
+            <div class="flex flex-col justify-between gap-3 text-lg items-center ">
+            <div>
+            <strong>Category:</strong> : ${product.category}
             </div>
+            <div>
+            <strong>Rating:</strong> : ${product.rating.rate}
+            </div>
+            <button 
+                class="add-to-cart bg-gradient-to-r from-[#FF6F3C] to-orange-500 text-white px-4 py-2 rounded-md font-semibold hover:opacity-90 cursor-pointer js-add-to-cart"
+            >
+                Add to Cart
+            </button>
+        </div>
             `
-        })
+    })
 
-          document.querySelectorAll('.js-add-to-cart').forEach(button => {
-          button.addEventListener('click', () => {
+    document.querySelectorAll('.js-add-to-cart').forEach(button => {
+      button.addEventListener('click', () => {
 
-              cartQuantity++;
-              document.querySelector('.cart-quantity').textContent = cartQuantity;
-              document.querySelector('.cart-quantity').classList.add('bg-red-500', 'text-white', 'text-sm' ,'rounded-full', 'w-6', 'h-6', 'pl-1')
-          });
+        cartQuantity++;
+        document.querySelector('.cart-quantity').textContent = cartQuantity;
+       
       });
     });
+  });
 
-    
 
-    
+
